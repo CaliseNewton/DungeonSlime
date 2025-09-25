a57cc4d17ac0cdcbdcaba0e4c0d567446e376305
CaliseNewton <newton@calise.dev>
2025-09-25 19:33:54 -0400

    Implement title and ggame scene, refactor Game1.cs

--- obj/Debug/net8.0/DungeonSlime.AssemblyInfo.cs
+++ obj/Debug/net8.0/DungeonSlime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51f66ea5c68e9b9de550bbbd461ca857a891f06c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b6ebf192586fb21eeaa350f3ab916084cfd4efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
