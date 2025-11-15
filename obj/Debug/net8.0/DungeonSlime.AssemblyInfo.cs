496579dd94c4f8e56b1b7742cacca945c7e32061
Calise Newton <newton@calise.dev>
2025-11-15 12:43:20 +1000

    Add scrolling title screen background.

--- obj/Debug/net8.0/DungeonSlime.AssemblyInfo.cs
+++ obj/Debug/net8.0/DungeonSlime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b6ebf192586fb21eeaa350f3ab916084cfd4efa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a57cc4d17ac0cdcbdcaba0e4c0d567446e376305")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
